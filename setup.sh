95a256ac429d17c32c30cc6c4d5dbf6a09312393
GunjanSkry <gunjan@improwised.com>
2019-04-08 17:27:16 +0530

    updating setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ aws --endpoint-url=http://localhost:4572 s3api create-bucket --bucket mytestbuck
 aws --endpoint-url=http://localhost:4572 s3 ls
 
 # Creates SQS Quese named user and todo
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name user
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name todo
+aws --endpoint-url=http://localhost:4576 sqs --region us-east-1 create-queue --queue-name user
+aws --endpoint-url=http://localhost:4576 sqs --region us-east-1 create-queue --queue-name todo
